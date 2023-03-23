c74a1fc9bc9d62ca1e128c4d3b3ccd99ebcc1025
argyaWicaksana <argya7@gmail.com>
2023-03-23 11:03:01 +0700

    initial commit

---
+++ test.py
@@ -0,0 +1,9 @@
+import requests
+
+api_key = '635213a8-5c90-4f07-9207-d1637912eceb'
+word = 'voluminous'
+root_url = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json'
+final_url = f'{root_url}/{word}?key={api_key}'
+r = requests.get(final_url)
+result = r.json()
+print(result)
